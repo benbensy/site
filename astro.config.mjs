babc31f9df26090b4659096ef2181c7bc025f0dd
benbensy <indusywu@qq.com>
2025-05-27 18:22:31 +0800

    fix: toc

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import sitemap from '@astrojs/sitemap'
 import icon from 'astro-icon'
 // @ts-check
 import { defineConfig } from 'astro/config'
+import remarkToc from 'remark-toc'
 import { transformerDirectives, transformerVariantGroup } from 'unocss'
 import unocss from 'unocss/astro'
 import presetWind3 from 'unocss/preset-wind3'
 import tsConfigPaths from 'vite-tsconfig-paths'
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
@@ -20,7 +22,10 @@ export default defineConfig({
   },
   markdown: {
     shikiConfig: {
-      theme: 'github-light'
-    }
-  }
+      theme: 'github-light',
+    },
+    remarkPlugins: [
+      [remarkToc, { heading: 'toc', maxDepth: 3 }],
+    ],
+  },
 })
--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,14 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  formatters: true,
   astro: true,
+  formatters: true,
+  typescript: true,
   rules: {
     'no-irregular-whitespace': 'off',
     'style/brace-style': 'off',
+    'style/no-trailing-spaces': 'off',
+    'antfu/if-newline': 'off',
   },
+  markdown: false,
 })
